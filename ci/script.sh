492d4524d2b277f5c1c20d924231676924419083
Jorge Aparicio <jorge@japaric.io>
2019-01-27 21:00:21 +0100

    ci: run test suite
    also update example

--- ci/script.sh
+++ ci/script.sh
@@ -5,8 +5,13 @@ main() {
 
     if [ $TARGET = x86_64-unknown-linux-gnu ]; then
         cargo check --target $TARGET --examples
+        cargo test --target $TARGET
         return
     fi
 }
+
+if [ -z ${TARGET-} ]; then
+    TARGET=$(rustc -Vv | grep host | cut -d ' ' -f2)
+fi
 
 main
