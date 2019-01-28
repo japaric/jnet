d68eafe5acef2d02e69e13c048117d8e79e87e5e
Jorge Aparicio <jorge@japaric.io>
2019-01-28 01:37:13 +0100

    remove more panicking branches from the ether API

--- panic-never/examples/ether.rs
+++ panic-never/examples/ether.rs
@@ -1,6 +1,9 @@
 #![feature(asm)]
+#![feature(maybe_uninit)]
 #![no_std]
 #![no_main]
+
+use core::mem::MaybeUninit;
 
 use cortex_m_rt::{entry, exception};
 use panic_never::force_eval;
@@ -9,18 +12,25 @@ use jnet::ether;
 
 const LEN: usize = 128;
 static mut BUFFER: [u8; LEN] = [0; LEN];
+static mut FRAME: MaybeUninit<ether::Frame<&'static mut [u8]>> = MaybeUninit::uninitialized();
 
 #[exception]
 unsafe fn SysTick() {
     if let Ok(f) = ether::Frame::parse(&mut BUFFER[..]) {
-        force_eval!(f.get_destination());
-        force_eval!(f.get_source());
-        force_eval!(f.get_type());
-        force_eval!(f.payload());
-        force_eval!(f.len());
+        FRAME.set(f);
     } else {
         asm!("NOP" : : : : "volatile");
     }
+}
+
+#[exception]
+unsafe fn SVCall() {
+    let f = FRAME.get_mut();
+
+    force_eval!(f.get_destination());
+    force_eval!(f.get_source());
+    force_eval!(f.get_type());
+    force_eval!(f.payload());
 }
 
 #[entry]
